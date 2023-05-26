ea010c8669100a3994ae47b30b43166b31a4d856
Kevyn0304 <kevyn0304@berkeley.edu>
2023-05-25 17:08:41 -0700

    progress from surface, pieces

--- chess/board.py
+++ chess/board.py
@@ -1,5 +1,5 @@
 import pygame
-from.constants import BLACK, ROWS, RED, SQUARE_SIZE
+from .constants import BLACK, ROWS, WHITE, SQUARE_SIZE
 
 class Board:
     def __init__(self):
@@ -11,5 +11,8 @@ class Board:
     def draw_squares(self, win):
         win.fill(BLACK)
         for row in range(ROWS):
-            for col in range(row & 2, ROWS, 2):
-                pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, WHITE, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def create_board(self):
+        pass
